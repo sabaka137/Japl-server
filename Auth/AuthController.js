41d2feb42604496d026686ff2d52308c26baa968
Stanislaw <animevbivza@gmail.com>
2023-11-23 14:03:15 +0300

    deploy-settings

--- Auth/AuthController.js
+++ Auth/AuthController.js
@@ -1,12 +1,30 @@
 const User = require("../Models/User");
-require('dotenv').config();
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose");
 
+
+const initialCollection = {
+  name: "First Collection",
+  description: "have a nice day",
+  termins: [
+    { id: 1, termin: "犬", meaning: "dog", reading: "いぬ" },
+    { id: 2, termin: "猫", meaning: "cat", reading: "ねこ" },
+    { id: 3, termin: "家", meaning: "house", reading: "いえ" },
+    { id: 4, termin: "人", meaning: "person", reading: "ひと" },
+    { id: 5, termin: "日本", meaning: "Japan", reading: "にほん" },
+    { id: 6, termin: "学校", meaning: "school", reading: "がっこう" },
+    { id: 7, termin: "食べる", meaning: "eath", reading: "たべる" },
+    { id: 8, termin: "見る", meaning: "look", reading: "みる" },
+    { id: 9, termin: "行く", meaning: "walk", reading: "いく" },
+    { id: 10, termin: "来る", meaning: "to come", reading: "くる" },
+  ],
+  id: new mongoose.Types.ObjectId()
+};
 
 function generateAccessToken(id) {
-
   return jwt.sign({ id: id }, process.env.JWT_SECRET_KEY, { expiresIn: "24h" });
 }
 
@@ -23,7 +41,7 @@ class AuthController {
         return res.status(400).json({ message: "User already exist" });
       }
       const hashPassword = bcrypt.hashSync(password, 7);
-      const user = new User({ ...req.body, password: hashPassword });
+      const user = new User({ ...req.body, password: hashPassword,groups:initialCollection});
 
       await user.save();
       const token = generateAccessToken(user._id.toString());
@@ -35,9 +53,9 @@ class AuthController {
   async login(req, res) {
     try {
       const { email, password } = req.body;
-      
+
       const user = await User.findOne({ email });
-    
+
       if (!user) {
         return res.status(400).json({ message: "user does not exist" });
       }
@@ -46,9 +64,9 @@ class AuthController {
       if (!validPassword) {
         return res.status(400).json({ message: "wrong password" });
       }
-     
+
       const token = generateAccessToken(user._id.toString());
-      console.log(token)
+      console.log(token);
       return res.status(200).json({ token });
     } catch (e) {
       res.status(400).json({ error: e });
--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function preloadDB() {
 
 const ioConfig = {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://japl.vercel.app",
     methods: ["GET", "POST"],
   },
 };
 
 const corsConfig = {
-  origin: ["http://localhost:3000"],
+  origin: ["https://japl.vercel.app"],
   credentials: true,
 };
 console.log(process.env.PORT)
